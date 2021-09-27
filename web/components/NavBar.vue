2743c8b8cd59a2a1e6b878e9813d5c9f38f0b253
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-09-27 14:36:27 +0000

    NavBar 완성 폴더 이름 수정

---
+++ web/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="nav">
+    <div class="nav__title">
+      <span>AI CARE</span>
+    </div>
+    <div class="nav__alarm">
+      <span><i class="far fa-bell"></i></span>
+    </div>
+  </nav>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.nav {
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 0px 0px 5px 5px;
+  // box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  box-shadow: 0px 3px 15px 2px rgba(155, 155, 155, 0.6);
+
+  &__title {
+    color: $yellow;
+    font-weight: bold;
+    font-size: 24px;
+  }
+
+  &__alarm {
+    font-size: 24px;
+    color: $gray;
+  }
+}
+</style>
---
+++ web/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+
+export default {
+  components: { NavBar },
+}
+</script>
