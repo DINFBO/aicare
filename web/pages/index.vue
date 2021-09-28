989ddfabac2a5fed7c4a3c072633559108927fa6
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-09-28 14:41:06 +0000

    main-page 디자인 변경

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -8,19 +8,6 @@
       </div>
     </div>
     <div class="main">
-      <!-- <div class="recent-diary">
-        <div class="title title--bold">
-          <span>최근 일기</span>
-        </div>
-        <ul>
-          <li>
-            <span class="recent-diary__title">오늘은 힘든 날이다...</span>
-            <span class="recent-diary__play-btn"
-              ><i class="far fa-play-circle"></i
-            ></span>
-          </li>
-        </ul>
-      </div> -->
       <div class="hits-posts">
         <div class="title title--bold">
           <span>인기글</span>
@@ -40,22 +27,6 @@
           </li>
         </ul>
       </div>
-      <!-- <div class="goto">
-        <div class="goto__write-diary">
-          <span class="goto__title">오늘은 어떤 일이 있으셨나요?</span>
-          <button class="goto__btn">일기 쓰러 가기</button>
-        </div>
-        <div class="goto__show-analysis">
-          <span class="goto__title"
-            >일기를 쓰셨다면 나의 감정변화를 확인해보세요.</span
-          >
-          <button class="goto__btn">분석결과 보러가기</button>
-        </div>
-        <div class="goto__comunity">
-          <span class="goto__title">다른 사람들과 고민을 나눠보세요</span>
-          <button class="goto__btn">커뮤니티로 가기</button>
-        </div>
-      </div> -->
       <div class="goto">
         <span class="goto__title">오늘은 어떤 일이 있으셨나요?</span>
         <span class="goto__btn">일기 쓰러 가기</span>
@@ -65,18 +36,16 @@
 </template>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap');
 
 .container {
-  // font-family: 'Nanum Pen Script', cursive;
   font-family: 'Gowun Dodum', sans-serif;
   box-sizing: border-box !important;
   padding: 32px 16px;
-  height: calc(100vh - 57px);
+  height: calc(100vh - 57px - 47px);
   .title {
-    font-size: 16px;
-    margin-bottom: 20px !important;
+    font-size: 18px;
+    margin-bottom: 24px !important;
 
     &--bold {
       font-weight: bold;
@@ -101,7 +70,7 @@
     &__img {
       display: flex;
       justify-content: center;
-      margin-top: 32px !important;
+      margin-top: 48px !important;
 
       div {
         width: 170px;
@@ -113,31 +82,13 @@
   }
 
   .main {
-    height: 30vh;
+    height: 35vh;
     min-height: 250px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: flex-start;
-    margin-top: 70px !important;
-  }
-
-  .recent-diary {
-    width: 100%;
-    box-sizing: border-box !important;
-    padding: 12px 16px;
-    // border: 1px solid rgb(147, 149, 151);
-    // border: 1px solid rgba(147, 149, 151, 0.3);
-    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-    border-radius: 5px;
-
-    ul {
-      li {
-        display: flex;
-        justify-content: space-between;
-        margin: 10px 0 !important;
-      }
-    }
+    margin-top: 90px !important;
   }
 
   .hits-posts {
