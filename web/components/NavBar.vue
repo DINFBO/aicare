6746e4aaa31e02dfe0d6e0d15c4c26236228f87f
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-09-29 13:09:54 +0000

    NavBar 경계선 설정

--- web/components/NavBar.vue
+++ web/components/NavBar.vue
@@ -24,8 +24,7 @@
   display: flex;
   justify-content: space-between;
   color: $gray;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px -2px 5px -1px,
-    rgba(0, 0, 0, 0.3) 0px -1px 3px -1px;
+  border-top: 1px solid $gray;
 
   &__icon {
     display: flex;
