d600c03a2b81ea213a7c540107dbf42364f598f7
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-01 07:59:54 +0000

    set-up v-calendar

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -15,7 +15,10 @@ export default {
   css: ['@/assets/reset.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/fontawesome.js'],
+  plugins: [
+    '@/plugins/fontawesome.js',
+    { src: '@/plugins/vcalendar.js', ssr: false },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ web/pages/history.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <no-ssr>
+      <v-date-picker
+        v-model="selectedDate"
+        mode="range"
+        :min-date="new Date()"
+        show-caps
+      />
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedDate: {
+        start: new Date(2018, 10, 9),
+        end: new Date(2018, 10, 10),
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+}
+</style>
