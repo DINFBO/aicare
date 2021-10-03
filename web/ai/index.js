357e9beb633aa45fbfe1e83749766eaa093d6479
JH.SUNG <86407466+DINFBO@users.noreply.github.com>
2021-10-03 08:44:05 +0000

    test tf.js

--- web/ai/index.js
+++ web/ai/index.js
@@ -1,3 +1,32 @@
+// One frame is ~23ms of audio.
+const NUM_FRAMES = 3;
+let examples = [];
+
+function collect(label) {
+ if (recognizer.isListening()) {
+   return recognizer.stopListening();
+ }
+ if (label == null) {
+   return;
+ }
+ recognizer.listen(async ({spectrogram: {frameSize, data}}) => {
+   let vals = normalize(data.subarray(-frameSize * NUM_FRAMES));
+   examples.push({vals, label});
+   document.querySelector('#console').textContent =
+       `${examples.length} examples collected`;
+ }, {
+   overlapFactor: 0.999,
+   includeSpectrogram: true,
+   invokeCallbackOnNoiseAndUnknown: true
+ });
+}
+
+function normalize(x) {
+ const mean = -100;
+ const std = 10;
+ return x.map(x => (x - mean) / std);
+}
+
 let recognizer;
 
 function predictWord() {
