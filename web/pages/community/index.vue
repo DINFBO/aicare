0d5944b3995368bf3c98f2e236da2f5699bcc7b6
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-05 14:31:29 +0000

    community 페이지 스타일링 및 타이틀과 글 작성 버튼 추가

--- web/pages/community/index.vue
+++ web/pages/community/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <PostItem />
+  <div class="container">
+    <div class="title">
+      <span>감정을 공유해보세요</span>
+    </div>
+    <div class="write">
+      <nuxt-link to="/community/write">작성하기</nuxt-link>
+    </div>
+    <PostItem v-for="i in 10" :key="i" />
   </div>
 </template>
 <script>
@@ -9,4 +15,32 @@ export default {
   components: { PostItem },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  box-sizing: border-box;
+  height: $page-height;
+  padding: 16px;
+  overflow-y: scroll;
+  position: relative;
+
+  .title {
+    font-size: $heading;
+    font-weight: bold;
+    padding-top: 16px;
+    margin-bottom: 36px;
+  }
+
+  .write {
+    padding: 8px 16px;
+    background-color: $yellow;
+    position: absolute;
+    top: 32px;
+    right: 16px;
+    border-radius: 5px;
+    a {
+      text-decoration: none;
+      color: $gray;
+    }
+  }
+}
+</style>
