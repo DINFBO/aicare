7a2f2b3a0be414f4cb194db0a5e50ebe945129dd
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-05 14:19:00 +0000

    PostItem 컴포넌트 스타일링

--- web/components/PostItem.vue
+++ web/components/PostItem.vue
@@ -6,10 +6,10 @@
       </div>
       <div class="post__create">
         <div class="post__author">
-          <img src="" alt="" />
+          <span class="author-img"><i class="far fa-user"></i></span>
           <span>김OO</span>
         </div>
-        <div class="post__created-at">2021-10-05</div>
+        <div class="post__timestamp">2021-10-05</div>
       </div>
     </div>
     <div class="post__recommend">
@@ -20,4 +20,49 @@
 <script>
 export default {}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.post {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  min-height: 50px;
+  margin: 24px 0;
+
+  div:first-child {
+    flex: 1;
+  }
+  &__title {
+    font-size: $sub-heading;
+    font-weight: bold;
+    margin-bottom: 8px;
+  }
+  &__create {
+    display: flex;
+    justify-content: space-between;
+    font-size: 16px;
+  }
+  &__author {
+    display:flex;
+    align-items: center;
+    .author-img {
+      font-size: 14px;
+      margin-right: 6px;
+    }
+  }
+
+  &__recommend {
+    box-sizing: border-box;
+    padding: 12px;
+    margin-left: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: $sub-heading;
+    font-weight: bold;
+    color: $white;
+    background-color: rgb(133, 211, 133);
+    border-radius: 10px;
+  }
+}
+</style>
