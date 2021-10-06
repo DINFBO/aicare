edfc60b6cdc458ffafa85e68085373c3931f5f95
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-06 14:30:22 +0000

    CommentItem 컴포넌트 제작

---
+++ web/components/CommentItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="comment">
+    <div class="comment__profile">
+      <div class="author">
+        <img src="" alt="" />
+        <span>박OO</span>
+      </div>
+      <span class="timestamp"> 2021-10-05 </span>
+    </div>
+    <div class="comment__content">
+      <span>제발 손절해... 정신에 해로울뿐인 동기는 절대네버</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.comment {
+  margin: 16px 0;
+
+  &__profile {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    margin-bottom: 6px;
+
+    .timestamp {
+      font-size: $description;
+      color: $gray;
+    }
+  }
+}
+</style>
