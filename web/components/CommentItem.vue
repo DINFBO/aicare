3d69cfaa9d0534ba501847080a5c544b65581fdf
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-07 13:39:25 +0000

    comunntiy 페이지 댓글 삭제 버튼 및 함수 추가

--- web/components/CommentItem.vue
+++ web/components/CommentItem.vue
@@ -8,13 +8,27 @@
       <span class="timestamp"> 2021-10-05 </span>
     </div>
     <div class="comment__content">
-      <span>제발 손절해... 정신에 해로울뿐인 동기는 절대네버</span>
+      <span>제발 손절해... 정신에 해로울뿐인 동기는 네버</span>
+      <template v-if="isCommentWritter">
+        <button @click="deleteComment"><i class="far fa-trash-alt"></i></button>
+      </template>
     </div>
   </div>
 </template>
+
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      isCommentWritter: true,
+    }
+  },
+  methods: {
+    deleteComment() {},
+  },
+}
 </script>
+
 <style lang="scss" scoped>
 .comment {
   margin: 12px 0;
@@ -44,6 +58,18 @@ export default {}
   &__content {
     word-break: keep-all;
     line-height: 1.1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    span {
+      max-width: 85%;
+    }
+    button {
+      border: none;
+      font-size: 16px;
+      color: #a83838;
+    }
   }
 }
 </style>
