82b01b48872615b811dfc93619a72f353cd75739
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-14 14:21:24 +0000

    _date.vue에 감정분석 컴포넌트 연결

--- web/pages/history/_date.vue
+++ web/pages/history/_date.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    hi
+    <EmotionalAnalysis :diary-url="diaryUrl" />
   </div>
 </template>
 <script>
+import EmotionalAnalysis from '../../components/EmotionalAnalysis.vue'
 export default {
-  
+  components: { EmotionalAnalysis },
+  async asyncData({ app, store, route }) {
+    const date = route.params.date
+    const uid = store.getters.getUid
+    const diaryRef = await app.$fire.storage.ref(`${uid}/diary/${date}`)
+    const diaryUrl = await diaryRef.getDownloadURL().then((url) => {
+      return url
+    })
+
+    return { diaryUrl }
+  },
 }
 </script>
-<style lang="scss" scoped>
-  
-</style>
+<style lang="scss" scoped></style>
