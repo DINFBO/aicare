59278298089859459d697339f1f8dffeffb0cebb
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-14 14:21:11 +0000

    감정분석 컴포넌트 초기화

---
+++ web/components/EmotionalAnalysis.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    감정분석 페이지
+    <button @click="play">재생</button>
+    <button @click="pause">정지</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    diaryUrl: {
+      type: String,
+      default() {
+        return ''
+      },
+    },
+  },
+  data() {
+    return {
+      audio: null,
+      playing: false,
+    }
+  },
+  methods: {
+    play() {
+      this.playing = true
+      this.audio = new Audio(this.diaryUrl)
+      this.audio.play()
+    },
+    pause() {
+      this.playing = false
+      this.audio.pause()
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+button {
+  padding: 16px;
+}
+</style>
