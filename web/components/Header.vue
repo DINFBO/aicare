4e6f89dca59cafc35ea38bc1439899a871c706fb
hwna00 <61866802+hwna00@users.noreply.github.com>
2021-10-16 14:47:07 +0000

    헤더에 반응형 추가

--- web/components/Header.vue
+++ web/components/Header.vue
@@ -4,9 +4,19 @@
       <div class="header__title">
         <NuxtLink to="/">AI CARE</NuxtLink>
       </div>
-      <div class="header__alarm">
-        <span @click="showAlarmModal"><i class="far fa-bell"></i></span>
-        <div v-if="isAlarmExist" class="alarm-circle"></div>
+      <div class="header__icon">
+        <div v-if="$breakpoints.lLg" class="menu">
+          <ul>
+            <li><nuxt-link to="/history">history</nuxt-link></li>
+            <li><nuxt-link to="/community">community</nuxt-link></li>
+            <li><nuxt-link to="/counseling">counseling</nuxt-link></li>
+            <li><nuxt-link to="/config">settings</nuxt-link></li>
+          </ul>
+        </div>
+        <div class="header__alarm">
+          <span @click="showAlarmModal"><i class="far fa-bell"></i></span>
+          <div v-if="isAlarmExist" class="alarm-circle"></div>
+        </div>
       </div>
     </header>
     <transition name="modalSlide">
@@ -53,6 +63,11 @@ export default {
     font-family: 'Caveat', cursive;
   }
 
+  &__icon {
+    display: flex;
+    align-items: center;
+  }
+
   &__alarm {
     font-size: $heading;
     position: relative;
@@ -65,6 +80,19 @@ export default {
       height: 7px;
       border-radius: 50%;
       background-color: $yellow;
+    }
+  }
+
+  .menu {
+    ul {
+      display: flex;
+      align-items: center;
+      margin-right: 16px;
+
+      li {
+        font-size: $sub-heading;
+        margin: 0px 24px;
+      }
     }
   }
 
